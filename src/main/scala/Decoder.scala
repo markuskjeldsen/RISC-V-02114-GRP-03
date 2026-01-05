8ab19952212c9daca6b3eea6453f07acf1ef4a4f
XXDragonslayer96XX <156065429+XXDragonslayer96XX@users.noreply.github.com>
2026-01-05 14:58:31 +0100

    ALU added

--- src/main/scala/Decoder.scala
+++ src/main/scala/Decoder.scala
@@ -1,3 +1,17 @@
 import chisel3._
 import chisel3.util._
 
+class decoder() extends Module{
+  val io = IO(new Bundle {
+    val input = Input(UInt(32.W))
+    val outputReg = Output(Uint(5.W))
+  })
+    val opcode := io.word(6,0)
+    val rd := io.word(11,7)
+    val f3 := io.word(14,12)
+    val rs1 := io.word(19,15)
+    val imm := io.word(31,20)
+    val io.outputReg := rd
+
+
+}
