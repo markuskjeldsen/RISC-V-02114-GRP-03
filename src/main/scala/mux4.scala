411664d79ce3c770a96084a9cc4a7da79dc3d303
markuskjeldsen <kinglink224@gmail.com>
2026-01-05 12:16:40 +0100

    i tried to make a mux4

---
+++ src/main/scala/mux4.scala
@@ -0,0 +1,27 @@
+import chisel3._
+import chisel3.util._
+
+class Multiplexer4() extends Module {
+    val io = IO(new Bundle {
+        val sel = Input(UInt(2.W))
+
+        val a0 = Input(UInt(8.W))
+        val a1 = Input(UInt(8.W))
+        val a2 = Input(UInt(8.W))
+        val a3 = Input(UInt(8.W))
+
+
+        val y = Output(Uint(8.W))
+    })
+
+    io.y := 1.U
+    switch(io.sel) {
+      is("b00".U) { io.y := io.a0 }
+      is("b01".U) { io.y := io.a1 }
+      is("b10".U) { io.y := io.a2 }
+      is("b11".U) { io.y := io.a3 }
+    }
+}
+
+
+
