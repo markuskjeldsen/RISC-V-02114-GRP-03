3800e685bd1c56f14c1b5e5cffa6e9c81e127b2d
markuskjeldsen <kinglink224@gmail.com>
2026-01-05 12:54:14 +0100

    make a VCD file

---
+++ src/test/scala/test.scala
@@ -0,0 +1,13 @@
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class test extends AnyFlatSpec with ChiselScalatestTester {
+  "HelloWorld " should "pass" in {
+    test(new HelloWorld(50000, 9600)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // Increase timeout to 5000 cycles
+      dut.clock.setTimeout(0)
+
+      dut.clock.step(1000)
+    }
+  }
+}
