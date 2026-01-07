65ec636ee2448a342280029a0ed675b73cb61db4
LasseBjernemose <lassebjernemose@gmail.com>
2026-01-07 13:12:58 +0100

    ALU comments

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -10,15 +10,15 @@ class ALU() extends Module{
   })
   io.out := 0.S
   switch(io.sel){
-    is(0.U) { io.out := io.a0 + io.a1 }
-    is(0.U) { io.out := io.a0 - io.a1 }
-    is(2.U) { io.out := io.a0 << io.a1(4,0)}
-    is(3.U) { io.out := (io.a0.asSInt < io.a1.asSInt).asUInt}
-    is(4.U) { io.out := (io.a0 < io.a1).asUInt}
-    is(5.U) { io.out := io.a0 ^ io.a1}
-    is(6.U) { io.out := io.a0 >> io.a1(4,0)}
-    is(7.U) { io.out := (io.a0 >> io.a1(4,0)).asUInt}
-    is(8.U) { io.out := io.a0 | io.a1 }
-    is(9.U) { io.out := io.a0 & io.a1 }
+    is(0.U) { io.out := io.a0 + io.a1 } //ADD
+    is(0.U) { io.out := io.a0 - io.a1 } //SUB
+    is(2.U) { io.out := io.a0 << io.a1(4,0)} //SLL
+    is(3.U) { io.out := (io.a0.asSInt < io.a1.asSInt).asUInt} //SLT
+    is(4.U) { io.out := (io.a0 < io.a1).asUInt} //SLTU
+    is(5.U) { io.out := io.a0 ^ io.a1} //XOR
+    is(6.U) { io.out := io.a0 >> io.a1(4,0)} //SRL
+    is(7.U) { io.out := (io.a0 >> io.a1(4,0)).asUInt} //SRA
+    is(8.U) { io.out := io.a0 | io.a1 } //OR
+    is(9.U) { io.out := io.a0 & io.a1 } //AND
   }
 }
