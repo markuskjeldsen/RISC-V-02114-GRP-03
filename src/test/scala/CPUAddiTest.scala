711ba9d001d876edfa479e9024022f17271ae11d
LasseBjernemose <lassebjernemose@gmail.com>
2026-01-08 14:30:09 +0100

    cpuaddit test expasion pack 1.0

--- src/test/scala/CPUAddiTest.scala
+++ src/test/scala/CPUAddiTest.scala
@@ -6,7 +6,7 @@ class CPUAddiTest extends AnyFlatSpec with ChiselScalatestTester {
     test(new CPU("src/test/scala/programs/addi.hex")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       // Increase timeout to inf cycles
       dut.clock.setTimeout(0)
-      dut.clock.step(20)
+      dut.clock.step(13)
       // addi x10, x0, 30
       // addi x0, x0, 0
       // addi x0, x0, 0
@@ -16,6 +16,17 @@ class CPUAddiTest extends AnyFlatSpec with ChiselScalatestTester {
       // addi x0, x0, 0
       // this program adds 30 and 34 into register x10
       dut.io.regs(10).expect(64)
+
+      dut.clock.step(13)
+      // addi x10, x0, -30
+      // addi x0, x0, 0
+      // addi x0, x0, 0
+      // addi x10, x10, -34
+      // addi x0, x0, 0
+      // addi x0, x0, 0
+      // addi x0, x0, 0
+      // this program adds -30 and -34 into register x10
+      dut.io.regs(10).expect(BigInt("FFFFFFC0", 16))
     }
   }
 }
