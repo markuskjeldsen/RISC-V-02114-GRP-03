fc3bb59998f5a14977ba6d65be84b59cc3437778
LasseBjernemose <lassebjernemose@gmail.com>
2026-01-13 14:54:32 +0100

    test for jal and lui

---
+++ src/test/scala/CPULuiTest.scala
@@ -0,0 +1,40 @@
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CPULuiTest extends AnyFlatSpec with ChiselScalatestTester {
+  "CPULuiTest" should "pass" in {
+    test(new CPU("src/test/scala/programs/Jal.hex")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // Increase timeout to inf cycles
+      dut.clock.setTimeout(0)
+      dut.clock.step(20)
+
+      dut.io.regs(10).expect(BigInt("0000a002",16)) //Test LUI
+
+      //_start:
+      //# Clear registers
+      //  addi x1, x0, 0      # ra = 0
+      //addi x5, x0, 0      # t0 = 0 (status)
+
+      //# Call function using JAL
+      //  jal  x1, jal_target
+      //addi x0, x0, 0
+      //addi x0, x0, 0
+
+      //addi x5, x0, 1      #Here if jalr works
+
+      //end:
+      //  jal x0, end               #infinite loop
+      //  addi x0, x0, 0
+      //addi x0, x0, 0
+
+      //jal_target:
+      //  addi x6, x0, 42     #Here if jal works
+
+      //#Return
+      //jalr x0, x1, 0
+
+
+
+    }
+  }
+}
