96aa47d40dbfe30ea3dcca2bf09e7d123455ef1a
LasseBjernemose <lassebjernemose@gmail.com>
2026-01-14 10:16:26 +0100

    jal test fixed

--- src/test/scala/CPUJalTest.scala
+++ src/test/scala/CPUJalTest.scala
@@ -11,13 +11,13 @@ class CPUJalTest extends AnyFlatSpec with ChiselScalatestTester {
       //dut.io.regs(10).expect(BigInt("00000001",16)) //Test Jalr
       dut.io.regs(11).expect(BigInt("0000002a",16)) //Test Jal
 
-      //_start:
+      //start:
       //# Clear registers
-      //  addi x1, x0, 0      # ra = 0
+      //  addi x10, x0, 0      # ra = 0
       //addi x5, x0, 0      # t0 = 0 (status)
 
       //# Call function using JAL
-      //  jal  x1, jal_target
+      //  jal  x10, jal_target
       //addi x0, x0, 0
       //addi x0, x0, 0
 
@@ -29,12 +29,10 @@ class CPUJalTest extends AnyFlatSpec with ChiselScalatestTester {
       //addi x0, x0, 0
 
       //jal_target:
-      //  addi x6, x0, 42     #Here if jal works
+      //  addi x11, x0, 42     #Here if jal works
 
       //#Return
-      //jalr x0, x1, 0
-
-
+      //jal x0, -16
 
     }
   }
