1985c2e8e7a0a9aae301be9c9ff37f32a898e532
LasseBjernemose <lassebjernemose@gmail.com>
2026-01-14 11:22:59 +0100

    forward test working

---
+++ src/test/scala/ForwardTest.scala
@@ -0,0 +1,19 @@
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ForwardTest extends AnyFlatSpec with ChiselScalatestTester {
+  "CPUForwardTest" should "pass" in {
+    test(new CPU("src/test/scala/programs/Forward.hex")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // Increase timeout to inf cycles
+      dut.clock.setTimeout(0)
+      dut.clock.step(30)
+
+      dut.io.regs(12).expect(BigInt("0000000C",16)) //Test Jalr
+
+      //addi x6, x0, 7
+      //addi x7, x0, 5
+      //addi x8, x0, 12
+
+    }
+  }
+}
