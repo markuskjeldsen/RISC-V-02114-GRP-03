23929501e49fc772cc661bd2151ae59ceae4a525
markuskjeldsen <kinglink224@gmail.com>
2026-01-15 12:13:23 +0100

    hazard works with load use

---
+++ src/test/scala/CPUHazardTest.scala
@@ -0,0 +1,25 @@
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CPUHazardTest extends AnyFlatSpec with ChiselScalatestTester {
+    "CPUHazard" should "pass" in {
+        test(new CPU("src/test/scala/programs/HazardLoadUse.hex")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            // Increase timeout to inf cycles
+            dut.clock.setTimeout(0)
+            dut.clock.step(72)
+            // addi x10, x0 , 30
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+            // sw x10, -4(sp)
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+            // lw x10, -4(sp) it should stall here
+            // add x11, x10, x0
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+            // addi x0, x0, 0
+        }
+    }
+}
