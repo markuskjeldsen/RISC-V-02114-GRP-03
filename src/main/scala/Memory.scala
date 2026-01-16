a43c2ded0ef9c2fd8208109f47798b7ba08871ea
markuskjeldsen <kinglink224@gmail.com>
2026-01-16 11:15:53 +0100

    added clear to memory

--- src/main/scala/Memory.scala
+++ src/main/scala/Memory.scala
@@ -5,6 +5,7 @@ import chisel3.util.experimental.loadMemoryFromFile
 class Memory(ProgPath: String, instMemWords: Int = 4096, dataMemWords: Int = 4096) extends Module {
   val io = IO(new Bundle {
     val instAddr = Input(UInt(32.W))
+    val instClear = Input(Bool())
     val inst     = Output(UInt(32.W))
 
     // Data memory
