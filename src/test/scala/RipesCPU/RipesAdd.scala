4e36f1c975c64d897d303f050002d13c9b317580
BjornvanOene <bjorn.vanoene@gmail.com>
2026-01-20 14:28:05 +0100

    RipesCPUTests

---
+++ src/test/scala/RipesCPU/RipesAdd.scala
@@ -0,0 +1,15 @@
+import chiseltest.{ChiselScalatestTester, WriteVcdAnnotation}
+import org.scalatest.flatspec.AnyFlatSpec
+
+class RipesAdd extends AnyFlatSpec with ChiselScalatestTester {
+  "RipesAdd" should "pass" in {
+    test(new CPU("src/test/scala/RipesTestPrograms/RipesAdd.hex",true)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      // Increase timeout to inf cycles
+      dut.clock.setTimeout(0)
+      dut.clock.step(22)
+
+
+    }
+  }
+}
+
